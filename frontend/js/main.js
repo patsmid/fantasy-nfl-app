820effebbb958cea8c1918d64c7f39a9555d5403
patsmid <patsmidnfl@gmail.com>
2025-07-03 17:58:17 -0600

    actualizacon a adminlte

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,21 +1,22 @@
 async function loadSidebar() {
   const sidebar = document.getElementById('sidebar');
-  sidebar.innerHTML = await (await fetch('/components/sidebar.html')).text();
-  $('.nav-sidebar').on('click','a[data-view]', function(e) {
-    e.preventDefault();
-    $('.nav-link').removeClass('active');
-    $(this).addClass('active');
-    const view = this.dataset.view;
-    loadView(view);
+  const response = await fetch('/components/sidebar.html');
+  sidebar.innerHTML = await response.text();
+
+  document.querySelectorAll('[data-view]').forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const view = link.getAttribute('data-view');
+      loadView(view);
+    });
   });
+
   loadView('players');
 }
 
-async function loadView(view) {
-  const module = await import(`./views/${view}.js`);
-  module.default();
+async function loadView(viewName) {
+  const viewModule = await import(`./views/${viewName}.js`);
+  viewModule.default();
 }
 
-$(function() {
-  loadSidebar();
-});
+loadSidebar();
