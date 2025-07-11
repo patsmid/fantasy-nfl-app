a0901fedcd4d9b1977c8ad6fc3c0eb6aa40a1bb7
Armando Jose Lopez Herrera <alopez@tsjyuc.gob.mx>
2025-07-11 12:30:00 -0600

    draft para redraft

--- backend/lib/draftUtils.js
+++ backend/lib/draftUtils.js
@@ -2,6 +2,7 @@ import { supabase } from '../supabaseClient.js';
 import fetch from 'node-fetch';
 import { getStarterPositions, getADPtype } from '../utils/helpers.js';
 import { sleeperADPcols, positions } from '../utils/constants.js';
+import { getNflState } from '../utils/sleeper.js';
 
 export async function getConfigValue(key) {
   const { data, error } = await supabase.from('config').select('value').eq('key', key).single();
@@ -31,13 +32,46 @@ export async function getPlayersData(playerIds) {
   return data;
 }
 
-export async function getRankings({ season, dynasty, scoring, idExpert, position }) {
+export async function getRankings_prev({ season, dynasty, scoring, idExpert, position }) {
   const week = 0;
   const posObj = positions.find(p => p.nombre === position) || positions.find(p => p.nombre === 'TODAS');
   const posValue = posObj.valor;
   const type = dynasty ? 'DK' : 'PRESEASON';
   const url = `https://partners.fantasypros.com/api/v1/expert-rankings.php?sport=NFL&year=${season}&week=${week}&id=${idExpert}&position=${posValue}&type=${type}&notes=false&scoring=${scoring}&export=json&host=ta`;
 	console.log(url);
+  const res = await fetch(url);
+  return await res.json();
+}
+
+export async function getRankings({ season, dynasty, scoring, idExpert, position, weekStatic = null }) {
+  const nflState = await getNflState();
+  let week = 0;
+
+  if (nflState.season_type === 'pre') {
+    week = 0;
+  } else {
+    week = nflState.week;
+  }
+
+  if (weekStatic !== null && weekStatic !== '') {
+    week = parseInt(weekStatic);
+  }
+
+  let pos = position;
+  if (position === 'TODAS' && (nflState.season_type === 'pre' || week === 0)) {
+    pos = 'TODAS_PRE';
+  }
+
+  const posObj = positions.find(p => p.nombre === pos) || positions.find(p => p.nombre === 'TODAS');
+  const posValue = posObj.valor;
+
+  let type = 'PRESEASON';
+  if (week > 0) type = 'WEEKLY';
+  if (dynasty) type = 'DK';
+
+  const url = `https://partners.fantasypros.com/api/v1/expert-rankings.php?sport=NFL&year=${season}&week=${week}&id=${idExpert}&position=${posValue}&type=${type}&notes=false&scoring=${scoring}&export=json&host=ta`;
+  console.log('📊 URL Rankings:', url);
+
   const res = await fetch(url);
   return await res.json();
 }
