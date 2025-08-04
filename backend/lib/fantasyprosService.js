60c0d35dd6ca2ca6a730df8b37443cd9bf7f2007
patsmid <patsmidnfl@gmail.com>
2025-08-04 13:52:57 -0600

    adp

--- backend/lib/fantasyprosService.js
+++ backend/lib/fantasyprosService.js
@@ -207,3 +207,34 @@ export function fuzzySearch(query, items, options = {}) {
 
   return results.length ? [results[0].item] : [];
 }
+
+function levenshtein(a, b) {
+  if (a === b) return 0;
+  if (!a || !b) return (a || b).length;
+
+  const matrix = [];
+
+  for (let i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+
+  for (let j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  for (let i = 1; i <= b.length; i++) {
+    for (let j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) === a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1, // sustitución
+          matrix[i][j - 1] + 1,     // inserción
+          matrix[i - 1][j] + 1      // eliminación
+        );
+      }
+    }
+  }
+
+  return matrix[b.length][a.length];
+}
