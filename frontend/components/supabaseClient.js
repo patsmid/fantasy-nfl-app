46da3d7c657f42d5d268f89db7110efa2de57da2
patsmid <patsmidnfl@gmail.com>
2025-08-19 01:56:03 -0600

    extras

--- frontend/components/supabaseClient.js
+++ frontend/components/supabaseClient.js
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+// supabaseClient.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey  = process.env.SUPABASE_ANON_KEY
+// ⚠️ Usa los valores de tu proyecto
+const supabaseUrl = "https://cdmesdcgkcvogbgzqobt.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkbWVzZGNna2N2b2diZ3pxb2J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NjM3ODIsImV4cCI6MjA2NzEzOTc4Mn0.QODh_sgbLeqNzYkXp8Ng3HflGaqBw5rf_sZHxanpZH8";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
