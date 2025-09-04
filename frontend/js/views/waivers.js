198619c67282478da994370fa0256424ae1caf90
Armando Jose Lopez Herrera <alopez@tsjyuc.gob.mx>
2025-09-04 15:08:50 -0600

    waivers pro v6

--- frontend/js/views/waivers.js
+++ frontend/js/views/waivers.js
@@ -529,7 +529,7 @@ export default async function renderWaiversView() {
 			const betterSamePos = samePos.filter(lp => rank < lp.rank).length;
 
 			betterBadge = `<i class="bi bi-graph-up-arrow text-warning ms-2"
-											  title="Mejor que ${comparisonCount}/${lineupFiltered.length} de tu roster • ${betterSamePos}/${samePos.length} en ${p.position}">
+											  title="Mejor que ${comparisonCount}/${lineupFiltered.length} de tu roster. • ${betterSamePos}/${samePos.length} en ${p.position}">
 											</i>`;
 	  }
 
